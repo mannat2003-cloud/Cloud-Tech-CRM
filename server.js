30caa5ad09410d1be6c7ed1092634895cd185d21
Mannat <itsmannat032@gmail.com>
2026-04-30 10:17:34 +0530

    fix deploy error

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
